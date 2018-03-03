0b73e00ff789e3bce96d578977c9e99c63de2a5d
Jonas Rotter <jonasrotter@gmail.com>
2018-03-03 15:19:11 +0100

    cleaned code

---
+++ lstm/train.py
@@ -0,0 +1,26 @@
+import logging
+import json
+import sys
+from data_utils import *
+from biLSTM import BiLSTM
+
+
+logging.getLogger().setLevel(logging.INFO)
+
+if len(sys.argv) != 2:
+    exit("Usage:\n python train.py [path to config file] ")
+
+
+config = json.loads(open(sys.argv[1]).read())
+x_embeddings, x, y, tags, voc, voc_inv, tag_dict_inv, sent_lens = load_processed_data(config['processed_data_location'])
+x_dev, tags_dev, sent_lens_dev = load_processed_data(config['processed_dev_data_location'], True)
+logging.info('Data loaded!')
+
+
+biLSTM =BiLSTM(config, tag_dict_inv, x.shape[1], x_embeddings, len(tag_dict_inv))
+biLSTM.train(x, tags, x_dev, tags_dev, sent_lens, sent_lens_dev)
+
+#tagger = Tagger()
+#tagger.train(train_data, n_epochs=3, trunc_data=None)
+#parser = Parser(tagger)
+#parser.train(train_data, n_epochs=3, do_projectivize=True, trunc_data=None)
